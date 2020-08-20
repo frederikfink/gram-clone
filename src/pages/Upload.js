79d119f6fcf9af7643b1831895ee744f8d6677a1
frfh <frfh@itu.dk>
2020-08-20 09:22:37 +0200

    Update Upload.js

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react'
 export class Upload extends Component {
     render() {
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                <button>Submit</button>
-                </form>
+            <div style={{width: "88%", margin: "auto"}}>
+                <div style={{display: "flex"}}> 
+                    <div className="uploadControls" style={{width: "30%", border: "1px solid red", borderRadius: "4px"}}>
+                        <div className="dragNDrop" >Drag n drop</div>
+                        <div className="manualUpload">choose file</div>
+                    </div>
+                    <div className="uploadPreview" style={{width: "70%", border: "1px solid red", borderRadius: "4px"}}>preview<br/>1000 x 1000 px</div>
+                </div>
+                <button>Upload</button>
             </div>
         )
     }
